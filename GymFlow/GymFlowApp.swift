aaf21c47e12a5fe1d9f974175c9766463749adbd
Daniel Hwang <dhwang154@ucsb.edu>
2024-10-14 15:07:24 -0700

    Initial Commit

---
+++ GymFlow/GymFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymFlowApp.swift
+//  GymFlow
+//
+//  Created by Daniel Hwang on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct GymFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
