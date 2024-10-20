c115e1dc87dbea11349c44a353e48c050deb31f5
Daniel Hwang <dhwang154@ucsb.edu>
2024-10-20 11:58:07 -0700

    Initial Commit

---
+++ PhoneAuth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhoneAuth
+//
+//  Created by Daniel Hwang on 10/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
